5e74c2c079a16dca736f80f5631c5c850f696b86
Karolis_portatil <karolisjakas04@gmail.com>
2024-01-28 15:24:33 +0100

    Commit Inicial

---
+++ Calculadora.java
@@ -0,0 +1,24 @@
+public class Calculadora {
+    private int numero1;
+    private int numero2;
+
+    public Calculadora(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public int realizarOperacion(String operacion) {
+        int resultado = 0;
+        switch (operacion) {
+            case "suma":
+                resultado = suma();
+                break;
+            // Add more cases for other operations if needed
+        }
+        return resultado;
+    }
+
+    public int suma() {
+        return numero1 + numero2;
+    }
+}
